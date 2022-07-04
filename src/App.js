31ed27152338d5f5e0719920124d9d1719e57f0e
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-07-04 10:47:15 +0100

    Update App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
 import './App.css';
-
+// https://api.coincap.io/v2/assets?limit=20
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Zahra's Crypro App </h1>
+      <div className="appcoin">
+        <article></article>
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </div>
   );
 }
